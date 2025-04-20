44212c6f105dece586095088d9de25ec6bf3bdec
Po-Sheng Cheng <bencer@outlook.com>
2025-04-20 17:45:47 -0400

    added test.py

--- export.py
+++ export.py
@@ -1,12 +1,17 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO('runs/detect/train10/weights/best.pt')  # pretrained YOLO11n model
+# Load a YOLOV8n PyTorch model
+model = YOLO('runs/detect/train10/weights/best.pt')
 
-# Run batched inference on a list of images
-results = model('img/*.jpg')  # return a list of Results objects
+# Export the model
+model.export(format="imx", data='/Users/poshengcheng/Documents/Spring 25/Grad Studio/toilet-vision-training/dataset/data.yaml')  # exports with PTQ quantization by default
 
-# Process results list
+# Load the exported model
+imx_model = YOLO("yolo8n_imx_model")
+
+# Run inference
+results = imx_model('img/*.jpg')
+
 for i, result in enumerate(results):
     boxes = result.boxes  # Boxes object for bounding box outputs
     masks = result.masks  # Masks object for segmentation masks outputs
